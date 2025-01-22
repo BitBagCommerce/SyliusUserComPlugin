2649b0cecc0431a7fb7231a1f0d6f7252f95a8d7
DominikWolos <dominik.wolos@bitbag.pl>
2025-01-22 11:16:33 +0100

    [UC-3] Move trait to plugin root & replace import namespace & add os header

--- src/Api/UserApi.php
+++ src/Api/UserApi.php
@@ -1,10 +1,17 @@
 <?php
 
+/*
+ * This file has been created by developers from BitBag.
+ * Feel free to contact us once you face any issues or want to start
+ * You can find more information about us on https://bitbag.io and write us
+ * an email on hello@bitbag.io.
+ */
+
 declare(strict_types=1);
 
 namespace BitBag\SyliusUserComPlugin\Api;
 
-use BitBag\SyliusUserComPlugin\Entity\UserComApiAwareInterface;
+use BitBag\SyliusUserComPlugin\Trait\UserComApiAwareInterface;
 use Symfony\Component\HttpFoundation\Request;
 
 final class UserApi extends AbstractClient implements UserApiInterface
@@ -76,7 +83,6 @@ final class UserApi extends AbstractClient implements UserApiInterface
                 'json' => $data,
                 'headers' => [
                     'Content-Type' => 'application/json',
-                    'User-Agent' => 'BitBag SyliusUserComPlugin',
                 ],
             ]),
         );
