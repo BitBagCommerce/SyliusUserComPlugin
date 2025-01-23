9d14e2a0c8f2380ddf8afa431299b7f0cfd5ecbb
DominikWolos <dominik.wolos@bitbag.pl>
2025-01-23 08:46:33 +0100

    [UC-3] Add OS header & return type

--- src/Form/Extension/UserComApiAwareTypeExtension.php
+++ src/Form/Extension/UserComApiAwareTypeExtension.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * This file has been created by developers from BitBag.
+ * Feel free to contact us once you face any issues or want to start
+ * You can find more information about us on https://bitbag.io and write us
+ * an email on hello@bitbag.io.
+ */
+
 declare(strict_types=1);
 
 namespace BitBag\SyliusUserComPlugin\Form\Extension;
@@ -14,7 +21,7 @@ use Symfony\Component\Validator\Constraints\Url;
 
 final class UserComApiAwareTypeExtension extends AbstractTypeExtension
 {
-    public function buildForm(FormBuilderInterface $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add(
