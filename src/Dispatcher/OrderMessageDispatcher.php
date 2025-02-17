2581f0dd5fee92490eca0900a324941604160a9d
DominikWolos <dominik.wolos@bitbag.pl>
2025-02-17 13:40:35 +0100

    Add log in event dispatcher

--- src/Dispatcher/OrderMessageDispatcher.php
+++ src/Dispatcher/OrderMessageDispatcher.php
@@ -12,6 +12,7 @@ declare(strict_types=1);
 namespace BitBag\SyliusUserComPlugin\Dispatcher;
 
 use BitBag\SyliusUserComPlugin\Message\OrderSynchronization;
+use Psr\Log\LoggerInterface;
 use Sylius\Component\Core\Model\OrderInterface;
 use Symfony\Component\Messenger\MessageBusInterface;
 
@@ -19,11 +20,18 @@ final class OrderMessageDispatcher implements OrderMessageDispatcherInterface
 {
     public function __construct(
         private readonly MessageBusInterface $messageBus,
+        private readonly LoggerInterface $logger
     ) {
     }
 
     public function dispatch(OrderInterface $order): void
     {
+        if (null === $order->getId()) {
+            $this->logger->error('Order ID cannot be null');
+
+            return;
+        }
+
         $this->messageBus->dispatch(new OrderSynchronization($order->getId()));
     }
 }
