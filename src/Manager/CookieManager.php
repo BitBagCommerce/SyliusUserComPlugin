7c25aafd95e717171e0544d8f37b5f519d4e3bcb
Tomasz Kaliński <tomkalon@gmail.com>
2025-10-02 10:30:57 +0200

    [UC-30] Introduce cookie queue handling and implement CookieFlusherSubscriber

--- src/Manager/CookieManager.php
+++ src/Manager/CookieManager.php
@@ -11,7 +11,9 @@ declare(strict_types=1);
 
 namespace BitBag\SyliusUserComPlugin\Manager;
 
+use BitBag\SyliusUserComPlugin\Cookie\CookieQueueInterface;
 use Sylius\Component\Core\Model\AdminUserInterface;
+use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
@@ -20,6 +22,8 @@ final class CookieManager implements CookieManagerInterface
     public function __construct(
         private readonly RequestStack $requestStack,
         private readonly TokenStorageInterface $tokenStorage,
+        private readonly CookieQueueInterface $queue,
+        private readonly ?string $cookieDomain = null,
     ) {
     }
 
@@ -41,11 +45,18 @@ final class CookieManager implements CookieManagerInterface
 
     public function setUserComCookie(string $value): void
     {
-        $request = $this->requestStack->getCurrentRequest();
-        if (null === $request) {
-            return;
+        $cookie = Cookie::create(self::CHAT_COOKIE_NAME)
+            ->withValue($value)
+            ->withPath('')
+            ->withSecure(true)
+            ->withHttpOnly(true)
+            ->withSameSite('lax');
+
+        if (null !== $this->cookieDomain && '' !== $this->cookieDomain) {
+            $cookie = $cookie->withDomain($this->cookieDomain);
         }
-        $request->cookies->set(self::CHAT_COOKIE_NAME, $value);
+
+        $this->queue->queue($cookie);
     }
 
     private function isShopUser(): bool
